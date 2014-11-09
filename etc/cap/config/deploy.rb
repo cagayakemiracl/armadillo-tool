e0621ece1c932c68cadf6c58c80400e219d8f093
cagayakemiracl <hirosnonno1182@gmail.com>
2014-11-09 15:02:31 +0900

    capistranoを使う準備

---
+++ etc/cap/config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
+
+project_dir = 'cd /media/sf_armadillo-tool && '
+
+task :build do
+  on roles(:atde) do
+    execute project_dir + 'make'
+  end
+end
+
+task :clean do
+  on roles(:atde) do
+    execute project_dir + 'make clean'
+  end
+end
+
+task :config do
+  on roles(:atde) do
+    execute project_dir + 'cmake .'
+  end
+end
